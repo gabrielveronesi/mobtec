6a317a38fdb17e0631850c310dfda40b3717cab4
Gabriel Veronesi <57780382+gabrielveronesi@users.noreply.github.com>
2023-02-22 03:30:34 -0300

    003

--- web/site/src/components/Banner.js
+++ web/site/src/components/Banner.js
@@ -1,8 +1,20 @@
 
 import banner from "../banner.jpg"
 import './Banner.css';
+import { useEffect, useState } from 'react';
+import ListaImoveis from "./ListaImoveis";
+
 
 const Banner = () => {
+
+    const [busca, setBusca] = useState('');
+
+    console.log(ListaImoveis.casas);
+    var casasNome = ['banana', 'uva' , 'rets'];
+    const casasFiltradas = casasNome.filter((casa) => casa.startsWith(busca));
+
+    console.log(casasFiltradas);
+
     return (
         <div className="banner d-flex align-items-center" style={{ backgroundImage: `url(${banner})` }}>
             <div className="bg-custom">
@@ -10,10 +22,16 @@ const Banner = () => {
                     <div className="row">
                         <div className="col-lg-6 mx-auto">
                             <div className="banner-area text-center pt-4 pb-4">
-                                <p>Busque o imóvel perfeiro para você!</p>
                                 <h2 className="mt-2 mb-4 banner-title"><strong> Pesquisar Imóvel</strong> </h2>
+                                <p>Busque por cidade, bairro ou endereço.</p>
+
                                 <div className="search-area">
-                                    <input type="text" className="inp-search" placeholder="Ex: Bairro Alto" />
+                                    <input type="text"
+                                        className="inp-search"
+                                        placeholder="Ex: São Paulo"
+                                        value={busca}
+                                        onChange={(ev) => setBusca(ev.target.value)}
+                                    />
                                     <button className="btn-search m-2">Procurar</button>
                                 </div>
                             </div>
